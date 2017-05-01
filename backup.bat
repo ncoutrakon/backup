a8364a4f200f907dcb2aa7ea1388422e0564262a
ncoutrakon <ncoutrakon@gmail.com>
2017-05-01 15:59:47 -0500

    backup.bat and load.bat(untested)

---
+++ backup.bat
@@ -0,0 +1,11 @@
+cd C:\SierraChart\
+copy *.xml C:\users\nick\documents\daytrader\backup\SierraChart
+copy *.config C:\users\nick\documents\daytrader\backup\SierraChart
+copy TradeActivityLog.dat C:\users\nick\documents\daytrader\backup\SierraChart
+copy SCStudiesDefSettings.ini C:\users\nick\documents\daytrader\backup\SierraChart
+copy Sierra3.cfg C:\users\nick\documents\daytrader\backup\SierraChart
+
+copy .\Data\*.cht C:\users\nick\documents\daytrader\backup\SierraChart\data\
+copy .\Data\*.twconfig C:\users\nick\documents\daytrader\backup\SierraChart\data\
+copy .\Data\*.SymbolList C:\users\nick\documents\daytrader\backup\SierraChart\data\
+copy .\Data\*.scss C:\users\nick\documents\daytrader\backup\SierraChart\data\
---
+++ load.bat
@@ -0,0 +1,5 @@
+
+cd C:\\Users\\Nick\\Documents\\daytrader\\backup\\
+
+copy .\\SierraChart\\ C:\\SierraChart\\
+copy .\\SierraChart\\data\\ C:\\SierraChart\\Data
